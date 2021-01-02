0a447593f9c0a4cb698d807f8d60285ef035eab9
Ilia_Kurenetc <ilya_kurenets@mail.ru>
2021-01-02 15:00:48 +0300

    My first Git commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+        System.out.println(add(a,b));
+    }
+
+    public static int add (int a, int b){
+        return a+b;
+    }
+}
